ba5229b68e45d1aaea35d15f1b981d609476ef62
Mary Jones <maryljones@gmail.com>
2013-04-06 02:30:27 -0700

    Ability to send sms on phone already connected to twitter implemented

--- TweaktheTweet/src/uw/changecapstone/tweakthetweet/TweetActivity.java
+++ TweaktheTweet/src/uw/changecapstone/tweakthetweet/TweetActivity.java
@@ -3,10 +3,16 @@ package uw.changecapstone.tweakthetweet;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TweetActivity extends Activity {
 
+	final static String SHORT_CODE = "40404";
+	String tweet;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -16,7 +22,22 @@ public class TweetActivity extends Activity {
 		TextView t = (TextView) findViewById(R.id.tweet_string);
 		
 		Intent intent = getIntent();
-		String tweet = intent.getStringExtra(MainActivity.TWEET_STRING);
+		tweet = intent.getStringExtra(MainActivity.TWEET_STRING);
 		t.setText(tweet);
 	}
+
+	public void sendTweet(View view) {
+		 try {
+				SmsManager smsManager = SmsManager.getDefault();
+				smsManager.sendTextMessage(SHORT_CODE, null, tweet, null, null);
+				Toast.makeText(getApplicationContext(), "SMS Sent!",
+							Toast.LENGTH_LONG).show();
+			  } catch (Exception e) {
+				Toast.makeText(getApplicationContext(),
+					"SMS faild, please try again later!",
+					Toast.LENGTH_LONG).show();
+				e.printStackTrace();
+			  }
+		 }
 }
+
