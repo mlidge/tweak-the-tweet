fffa58c50306569b7447220caaae35370687cea8
Alan Loh <yaluen@uw.edu>
2013-05-01 04:37:00 -0700

    merged the list functionality into main code. Still need to polish some things

--- TweaktheTweet/src/uw/changecapstone/tweakthetweet/TestStringBuilder.java
+++ TweaktheTweet/src/uw/changecapstone/tweakthetweet/TestStringBuilder.java
@@ -5,22 +5,27 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.app.AlertDialog;
 
 public class TestStringBuilder extends Activity implements OnItemSelectedListener {
 
-	private Spinner disaster_tag_spinner, event_tag_spinner;
+	private Spinner disaster_tag_spinner;
+	private ListView disaster_list;
 	private String disaster_tag;
-	private EditText test_string, city_name;
+	private EditText city_name, test_tweet;
 	private String tweet;
 	
 	@Override
@@ -29,13 +34,26 @@ public class TestStringBuilder extends Activity implements OnItemSelectedListene
 		setContentView(R.layout.activity_test_string_builder);
 		
 		disaster_tag_spinner = (Spinner) findViewById(R.id.disaster_tag_spinner);
-		event_tag_spinner = (Spinner) findViewById(R.id.event_tag_spinner);
 		city_name = (EditText) findViewById(R.id.city_name);
-		test_string = (EditText) findViewById(R.id.test_text);
+		test_tweet = (EditText) findViewById(R.id.test_tweet);
+		disaster_list = (ListView) findViewById(R.id.list);
+		disaster_tag = "";
 		
 		// Set Click Listener
 		disaster_tag_spinner.setOnItemSelectedListener(this);
 
+		disaster_list.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> adapter, View v, int position,
+					long id) {
+				
+				disaster_tag = (String) adapter.getItemAtPosition(position);
+				test_tweet.setText(disaster_tag);
+			}
+			
+			
+		});
 	}
 
 	@Override
@@ -49,30 +67,36 @@ public class TestStringBuilder extends Activity implements OnItemSelectedListene
 	@Override
 	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
 			long arg3) {
-		//addEventTagsOnSpinner();
-		List<String> listA = new ArrayList<String>();
-		List<String> listB = new ArrayList<String>();
-		listA.add("Alpha");
-		listA.add("Beta");
-		listA.add("Gamma");
-		listB.add("One");
-		listB.add("Two");
-		listB.add("Three");
+//		//addEventTagsOnSpinner();
+//		List<String> listA = new ArrayList<String>();
+//		List<String> listB = new ArrayList<String>();
+//		listA.add("Alpha");
+//		listA.add("Beta");
+//		listA.add("Gamma");
+//		listB.add("One");
+//		listB.add("Two");
+//		listB.add("Three");
+//		
+//		ArrayAdapter<String> dataAdapter = null;
+//		if (disaster_tag_spinner.getSelectedItem().toString().equals("Earthquake") &&
+//				city_name.getText().toString().equals("Seattle")){
+//			dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listA);
+//			dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//			event_tag_spinner.setAdapter(dataAdapter);
+//			
+//		} else if (disaster_tag_spinner.getSelectedItem().toString().equals("Flood") &&
+//				city_name.getText().toString().equals("Seattle")){
+//			dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listB);
+//			dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//			event_tag_spinner.setAdapter(dataAdapter);
+//		}
 		
-		ArrayAdapter<String> dataAdapter = null;
-		if (disaster_tag_spinner.getSelectedItem().toString().equals("Earthquake") &&
+		//Create adapter
+		if (disaster_tag_spinner.getSelectedItem().toString().equals("Earthquake") && 
 				city_name.getText().toString().equals("Seattle")){
-			dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listA);
-			dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-			event_tag_spinner.setAdapter(dataAdapter);
-			
-		} else if (disaster_tag_spinner.getSelectedItem().toString().equals("Flood") &&
-				city_name.getText().toString().equals("Seattle")){
-			dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listB);
-			dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-			event_tag_spinner.setAdapter(dataAdapter);
+			ListAdapter adapter = createAdapter();
+			disaster_list.setAdapter(adapter);
 		}
-		
 	}
 
 	@Override
@@ -80,6 +104,17 @@ public class TestStringBuilder extends Activity implements OnItemSelectedListene
 		// TODO Auto-generated method stub
 		
 	}
+	
+//	//Automatically switch to next screen no matter what button is clicked (for test mockup purposes)
+//    @Override 
+//    public void onListItemClick(ListView l, View v, int position, long id) {
+//    	tweet += l.getSelectedItem().toString();
+//		Intent i = new Intent(this, TestStringBuilderCategory.class);
+//		i.putExtra("tweet", tweet);
+//		startActivity(i);
+//        //Intent i = new Intent(getApplicationContext(), LocationActivity.class);
+//        //startActivity(i);
+//    }
 	
 	public void addDisasterOnSpinner() {
 		
@@ -91,58 +126,51 @@ public class TestStringBuilder extends Activity implements OnItemSelectedListene
 		disaster_tag_spinner.setAdapter(dataAdapter);
 	}
 	
-	public void addEventTagsOnSpinner() {
-		List<String> listA = new ArrayList<String>();
-		List<String> listB = new ArrayList<String>();
-		listA.add("Alpha");
-		listA.add("Beta");
-		listA.add("Gamma");
-		listB.add("One");
-		listB.add("Two");
-		listB.add("Three");
-		
-		ArrayAdapter<String> dataAdapter = null;
-		if (disaster_tag_spinner.getSelectedItem().toString().equals("Earthquake") &&
-				city_name.getText().toString().equals("Seattle")){
-			dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listA);
-			
-		} else if (disaster_tag_spinner.getSelectedItem().toString().equals("Flood") &&
-				city_name.getText().toString().equals("Seattle")){
-			
-			dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listB);
-		}
-		
-		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		event_tag_spinner.setAdapter(dataAdapter);
-		
-	}
-	
-	public void buildString(View view) {
-		//disaster_tag = disaster_tag_spinner.getSelectedItem().toString();
-		//disaster_description_string = disaster_description.getText().toString();
-		//test_string.setText("#"+disaster_tag + " " + disaster_description_string, TextView.BufferType.NORMAL);
-
-	}
 	
 	public void nextViewCategory(View view){
-		if (event_tag_spinner.getSelectedItem()==null){
-			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-			alertDialog.setTitle("Error");
-			alertDialog.setMessage("Please select an event");
-			// -1 = BUTTON_POSITIVE = a positive button?
-			alertDialog.setButton(-1, "OK", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					// here you can add functions
-				}
-			});
-			alertDialog.show();
-		} else {
-			tweet += event_tag_spinner.getSelectedItem().toString();
-			Intent i = new Intent(this, TestStringBuilderCategory.class);
-			i.putExtra("tweet", tweet);
-			startActivity(i);
-		}
+//		if (event_tag_spinner.getSelectedItem()==null){
+//			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+//			alertDialog.setTitle("Error");
+//			alertDialog.setMessage("Please select an event");
+//			// -1 = BUTTON_POSITIVE = a positive button?
+//			alertDialog.setButton(-1, "OK", new DialogInterface.OnClickListener() {
+//				public void onClick(DialogInterface dialog, int which) {
+//					// here you can add functions
+//				}
+//			});
+//			alertDialog.show();
+//		} else {
+		tweet += disaster_tag;
+		Intent i = new Intent(this, TestStringBuilderCategory.class);
+		i.putExtra("tweet", tweet);
+		startActivity(i);
+//		}
 	}
+	
+	protected ListAdapter createAdapter()
+    {
+    	//Test data for initial event list
+    	//Read this in from somewhere else
+    	String[] testData = new String[] {
+    			"#TestEvent1\n\tDescription1",
+    			"#TestEvent2\n\tDescription2",
+    			"#TestEvent3\n\tDescription3",
+    			"#TestEvent4\n\tDescription4",
+    			"#TestEvent5\n\tDescription5",
+    			"#TestEvent6\n\tDescription6",
+    			"#TestEvent7\n\tDescription7",
+    			"#TestEvent8\n\tDescription8",
+    			"#TestEvent9\n\tDescription9",
+    			"#TestEvent10\n\tDescription10",
+    			"#TestEvent11\n\tDescription11",
+    			"#TestEvent12\n\tDescription12",
+    	};
+ 
+    	// Create a String array adapter using the testData values
+    	ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, testData);
+ 
+    	return adapter;
+    }
 
 
 }
